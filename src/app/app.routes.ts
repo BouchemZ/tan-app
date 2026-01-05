b2f5763c133d7b8f03737775b149fd33b18c47a8
Laptop <bouchemazakary@gmail.com>
2026-01-05 22:50:10 +0100

    refacto header and footer. add components pages services. add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Timer } from './pages/timer/timer';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'profile', component: Profile },
+  { path: 'timer', component: Timer }
+];
